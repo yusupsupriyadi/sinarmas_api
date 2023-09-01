d55fa52548ff969dde764cea9800b846a64f0726
TheGodFathers <yusup.kejarid@gmail.com>
2023-09-01 15:56:08 +0700

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return Task::all();
+    }
+
+    public function detail($id)
+    {
+        $task = Task::find($id);
+
+        if (!$task) {
+            return response()->json(['message' => 'Task not found'], 404);
+        }
+
+        return response()->json($task, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $task = new Task;
+        $task->title = $request->input('title');
+        $task->description = $request->input('description');
+        $task->save();
+
+        return response()->json(['message' => 'Task created successfully'], 201);
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+
+        if (!$task) {
+            return response()->json(['message' => 'Task not found'], 404);
+        }
+
+        $task->delete();
+
+        return response()->json(['message' => 'Task deleted successfully'], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $task = Task::find($id);
+
+        if (!$task) {
+            return response()->json(['message' => 'Task not found'], 404);
+        }
+
+        $task->title = $request->input('title');
+        $task->description = $request->input('description');
+        $task->save();
+
+        return response()->json(['message' => 'Task updated successfully'], 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\TaskController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('tasks', [TaskController::class, 'index']);
+Route::get('tasks/{id}', [TaskController::class, 'detail']);
+Route::post('tasks', [TaskController::class, 'store']);
+Route::post('tasks/{id}', [TaskController::class, 'update']);
+Route::delete('tasks/{id}', [TaskController::class, 'destroy']);
